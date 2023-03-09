8c6498c0ee1493e75ebf6057e95a85b27456bb5f
Andrew <andrewvo1024@gmail.com>
2023-03-09 09:50:00 -0600

    Css changes to project video

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -2,3 +2,24 @@ function scrollToContact() {
     let projectsSection = document.getElementById("contactMe");
     projectsSection.scrollIntoView({ behavior: 'smooth' });
   }
+
+
+// Get the section element and the video element
+const twitterSection = document.getElementById('twitter-section');
+const twitterVideo = twitterSection.querySelector('video');
+
+// Add an event listener to the section element for mouseenter and mouseleave events
+twitterSection.addEventListener('mouseenter', () => {
+    // Set the src attribute of the video element to the URL of the video
+    twitterVideo.src = 'videos/twitter screen recording.mov';
+    // Play the video
+    twitterVideo.play();
+});
+
+twitterSection.addEventListener('mouseleave', () => {
+    // Pause the video
+    twitterVideo.pause();
+    // Set the src attribute of the video element to an empty string to stop the video from playing
+    twitterVideo.src = '';
+    twitterVideo.setAttribute('poster', poster);
+});
